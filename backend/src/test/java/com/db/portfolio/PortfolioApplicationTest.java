90fdb910eed23644cedf0f047aee432ea699b896
Dimitris Bouzoukas <dimbouzoukas@gmail.com>
2023-03-20 19:38:04 +0200

    Initial commit backend

---
+++ backend/src/test/java/com/db/portfolio/PortfolioApplicationTest.java
@@ -0,0 +1,13 @@
+package com.db.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
