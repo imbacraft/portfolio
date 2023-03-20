8e2a163894825d33409bfa9aa69393e5b47cc348
Dimitris Bouzoukas <dimbouzoukas@gmail.com>
2023-03-20 20:38:36 +0200

    Controller

---
+++ backend/src/main/java/com/db/portfolio/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.db.portfolio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/** HomeController */
+@RequestMapping("/")
+public class HomeController {
+
+  public String homeDisplay() {
+
+    return "";
+  }
+}
